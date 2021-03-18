f04c67ca86a7650aa7786a6f74a42e49267fba52
MrPaul007 <75275133+MrPaul007@users.noreply.github.com>
2021-03-18 10:18:20 +0100

    ---

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,1 @@
   
-function fetchCountries(countryName) {
-    return fetch(
-      `https://restcountries.eu/rest/v2/name/${countryName}`,
-    ).then(data => data.json());
-  }
-  
-  export default fetchCountries;
