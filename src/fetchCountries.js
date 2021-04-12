4f3d14b1fd2d23eead30060579547821b9abc268
MrPaul007 <75275133+MrPaul007@users.noreply.github.com>
2021-04-12 22:11:44 +0300

    reloaded

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,7 @@
+function fetchCountries(countryName) {
+    return fetch(
+      `https://restcountries.eu/rest/v2/name/${countryName}`,
+    ).then(res => res.json());
+  }
   
+  export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,75 @@
 import './styles.css';
+import debounce from 'lodash.debounce';
+
+import { notice, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+import fetchCountries from './fetchCountries.js';
+import coutryEl from './countryTemp.hbs';
+
+const innerCountry = document.querySelector('.inner_country');
+const input = document.querySelector('#input');
+const listCountries = document.querySelector('.list_countries');
+
+function createCountry(e) {
+  listCountries.innerHTML = '';
+  innerCountry.innerHTML = '';
+
+  const value = e.target.value;
+  if (!value) {
+    return;
+  }
+
+  function makeMarkup() {
+    fetchCountries(value)
+      .then(data => {
+        data.forEach(el => {
+          if (data.length > 1) {
+            let liEl = '';
+            liEl += `<li class="item_countries">${el.name}</li>`;
+            listCountries.insertAdjacentHTML('beforeend', liEl);
+
+            innerCountry.innerHTML = '';
+
+            listCountries.addEventListener('click', e => {
+              console.log(e.target.textContent);
+              if (e.target.textContent === el.name) {
+                innerCountry.innerHTML = coutryEl(el);
+                listCountries.innerHTML = '';
+              }
+            });
+          } else if (data.length) {
+            data.forEach(el => {
+              innerCountry.innerHTML = coutryEl(el);
+            });
+          }
+        });
+      })
+      .catch(errorMessae);
+  }
+
+  function notification() {
+    notice({
+      text: ' Введите больше символов',
+      delay: 1000,
+    });
+  }
+
+  function errorMessae() {
+    error({
+      text: 'Такой страны не существует',
+      delay: 1000,
+    });
+  }
+
+  fetchCountries(value).then(data => {
+    if (data.length !== 0 && data.length > 10) {
+      notification();
+    } else {
+      makeMarkup();
+    }
+  });
+}
+
+input.addEventListener('input', debounce(createCountry, 500));
